7aec00382615566189231ce78fa6e5fefebcc870
DH_1_KA <putraadhika15@gmail.com>
2025-06-21 14:43:43 +0700

    fix query

--- node/config/database.js
+++ node/config/database.js
@@ -1,25 +1,22 @@
-const Pool = require("pg").Pool;
+const { Pool } = require("pg");
 const pool = new Pool({
     user: 'postgres',
     host: 'localhost',
     database: 'testbe',
     password: 'postgres',
     port: 5432
-});console.log(pool);
+});
 
 
 const getReservationByDate = (request, response) => {
-    // const user_id = parseInt(request.body.user_id)
-    // const party_size = parseInt(request.body.party_size)
-    // const requested_date = request.body.requested_date
-    // const requested_time = request.body.requested_time
-    // const duration_minutes = request.body.duration_minutes
-    const { user_id, party_size, requested_date, requested_time, duration_minutes } = request.body
-    pool.query('select * from reservations limit 1', (error, results) => {
-        if (error) {
-            throw error
-        }
-        response.status(200);
+    const user_id = parseInt(request.body.user_id)
+    const party_size = parseInt(request.body.party_size)
+    const requested_date = request.body.requested_date
+    const requested_time = request.body.requested_time
+    const duration_minutes = request.body.duration_minutes
+    
+    pool.query('Select r.table_id from reservations r join users u on u.id = r.user_id join orders o on o.reservation_id = r.id where u.id = $1', [user_id], (error, results) => {                
+        return response.status(200)
     })
 }
 
